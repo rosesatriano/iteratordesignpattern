2d7a26343de0f21b682fb0eca2c754ea5a4a2f9b
rosesatriano <satrianr@email.sc.edu>
2021-02-15 23:49:51 -0500

    comments

--- iteratordesignpattern/Item.java
+++ iteratordesignpattern/Item.java
@@ -1,24 +1,47 @@
 package iteratordesignpattern;
 
+/**
+ * In this class, we are holding the details of an item. This later prints the
+ * item name, item description, and item price. 
+ * @author rosesatriano
+ */
 public class Item{
     private String title;
     private String description;
     private double price;
     
+    /**
+     * This constructor initializes the variables to specific values.
+     * @param title title of item
+     * @param description description of item
+     * @param price price of item
+     */
     public Item(String title, String description, double price){
         this.title = title;
         this.description = description;
         this.price = price;
     }
     
+    /**
+     * This method returns the string representation of item that includes the title,
+     * the description, and the price.
+     */
     public String toString(){
         return "***** "+title+" *****\n "+description+"\n Price: $"+price+"\n";
     }
 
+    /**
+     * This method returns the price of the item. 
+     * @return price 
+     */
     public double getPrice(){
         return price;
     }
 
+    /**
+     * This method displays the item details including the title, description,
+     * and price.
+    */
     public void print(){
         System.out.println("***** "+title+" *****");
         System.out.println(description);
--- iteratordesignpattern/WishListIterator.java
+++ iteratordesignpattern/WishListIterator.java
@@ -2,20 +2,41 @@ package iteratordesignpattern;
 
 import java.util.Iterator;
 
+/**
+ * This class implements the java.util iterator, so we can iterate the items
+ * in the wishlist.
+ * @author rosesatriano
+ */
 public class WishListIterator implements Iterator <Item>{
     private Item[] items;
     private int position = 0;
 
+    /**
+     * This constructor creates a new iterator for the items at the start.
+     * @param items
+     */
     public WishListIterator(Item[] items){
         this.items = items;
     }
 
+    /**
+     * This method moves through the list of items by using position. 
+     * @param position allows the iterator to get the item at that specific 
+     * position
+     * @return This returns the next item in the iterator.
+     */
     public Item next(){
         Item item = items[position];
         position = position + 1;
         return item;
     }
 
+    /**
+     * This method determines whether if there are items to iterate or not.
+     * @return false is when there is no more items to iterate when we reach
+     * the end of the list or if there is no item in that position. 
+     * @return true when there are items to be iterated.
+     */
     public boolean hasNext(){
         if(position >= items.length || items[position] == null){
             return false;
